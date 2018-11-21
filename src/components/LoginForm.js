b5f01971172195a5bde97dbf97ecdf92fd953224
github username <github email address>
2018-11-21 10:51:09 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsername = (event) =>  {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePassWord = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) return
+    this.props.onSubmit(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event=> this.handleUsername(event)} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event=> this.handlePassWord(event)} value={this.state.password} />
           </label>
         </div>
         <div>
